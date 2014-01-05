fa9e2c9a6265da19fe2b7c70af1f68e5d2e513f8
Wafiq Rodzuan <wafiqrodzuan@gmail.com>
2014-01-05 16:10:41 +0800

    Auto build

--- config.rb
+++ config.rb
@@ -60,6 +60,7 @@ configure :build do
 end
 
 activate :deploy do |deploy|
+  deploy.build_before = true
   deploy.method = :git
   # Optional Settings
   # deploy.remote   = "custom-remote" # remote name or git url, default: origin
